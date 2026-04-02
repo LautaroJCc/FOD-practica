1401082beb62410b0db3f8bf7f61a15d9cfd5615
LautaroJCc <lautarojoaquin006@gmail.com>
2026-04-02 15:17:08 -0300

    Update fod02.pas

--- practica 2/fod02.pas
+++ practica 2/fod02.pas
@@ -19,6 +19,9 @@ NOTA: Para la actualización del inciso a) los archivos deben ser recorridos só
 
 program ejercicio2;
 
+const
+  valor_grande = 9999;
+
 type
   alumno = record
     cod, cant_sin_final, cant_con_final: integer;
@@ -34,56 +37,69 @@ type
   archivo2 = file of alu2;
   texto = text;
   
-  procedure cargar_archivos(var arch: archivo; var arch2: archivo2); //se dispone --- esto se pone?
+  //procedure cargar_archivos(var arch: archivo; var arch2: archivo2); //se dispone --- esto se pone?
   
-  procedure actualizar(var arch: archivo; var arch2: archivo2; num: integer);
-    var 
-      cont: integer;
-      dat, act: alu2;
-      dat2: alumno;    
+  procedure leer1(var arch: archivo; var a: alumno);
+    begin
+      if (not(EOF(arch))) then
+        read(arch, a)
+      else
+        a.cod := valor_grande;
+    end;
+    
+  procedure leer2(var arch: archivo2; var a: alu2);
+    begin
+      if (not(EOF(arch))) then
+        read(arch, a)
+      else
+        a.cod := valor_grande;
+    end;
+  
+  procedure actualizar(var arch: archivo; var arch2: archivo2);
+    var
+      dat: alumno;
+      dat2: alu2; 
+      aprobo, desaprobo, act_cod: integer;
+      
     begin
       reset(arch);
       reset(arch2);
       
-      if (not(EOF(arch2))) then
+      leer1(arch, dat);
+      leer2(arch2, dat2);
+      while (dat2.cod <> valor_grande) do
         begin
-          read(arch2, dat);
-      
-	      while (not(EOF(arch2))) do
-	        begin
-	          cont := 0;
-	          act := dat;
-	          
-	          while (not(EOF(arch2))) and (dat.cod = act.cod) do
-	            begin
-	              case (num) of 
-	                1: begin
-	                     if (dat.aprobo) then
-	                       cont := cont +1;
-	                   end;
-	                2: begin
-	                     if (dat.aprobo = false) then
-	                       cont := cont +1;
-	                   end;
-	               end;
-	               
-	               read(arch2, dat); //sig
-	            end;
-	          //salgo 
-	          read(arch, dat2);
-	          while (dat2.cod <> act.cod) and (not EOF(arch)) do //porque en el archivo detalle puede haber 0, 1 o más registros por cada alumno del archivo maestro
-                read(arch, dat2);
-	          if (num = 1) then
-	            dat2.cant_con_final := dat2.cant_con_final + cont
-	          else
-	            dat2.cant_sin_final := dat2.cant_sin_final + cont;
-	          seek(arch, filepos(arch)-1);
-	          write(arch, dat2);       
-	        end;      
+          act_cod := dat2.cod;
+          aprobo := 0;
+          desaprobo := 0;
+          
+          while (act_cod = dat2.cod) do
+            begin
+              if (dat2.aprobo) then
+                aprobo := aprobo +1
+              else
+                desaprobo := desaprobo +1;
+              
+              leer2(arch2, dat2);
+            end;            
+          while (dat.cod <> act_cod) do
+            leer1(arch, dat);
+              
+          if (dat.cod <> valor_grande) then
+            begin
+              if (aprobo > 0) then
+                dat.cant_con_final := dat.cant_con_final + aprobo
+              else if (desaprobo > 0) then
+                dat.cant_sin_final := dat.cant_sin_final + desaprobo;
+            end;
+            
+          // Vuelvo una posición atrás para sobreescribir el registro correcto
+          seek(arch, filepos(arch) - 1);
+          write(arch, dat);
         end;
       close(arch);
       close(arch2);
-    end;
+    end; 
     
   procedure puntoB(var arch: archivo; var arch2: texto);
     var 
@@ -92,37 +108,29 @@ type
       reset(arch);
       rewrite(arch2);
       
-      while (not(EOF(arch))) do
+      leer1(arch, dat);
+      while (dat.cod <> valor_grande) do
         begin
-          read(arch, dat);
-   
           if (dat.cant_sin_final > 4) then
             writeln(arch2, dat.cod, ' ', dat.cant_sin_final, ' ', dat.cant_con_final, ' ', dat.ape, ' ', dat.nom);
+            
+          leer1(arch, dat);
         end;      
       close(arch);
       close(arch2);
     end;
     
-  
 var
   maestro: archivo;
   detalle: archivo2;
   txt: texto;
-  topico: 'a'..'b';
-  num: 1..2;
   
 begin
   assign(maestro, 'maestro');
   assign(detalle, 'detalle');
   assign(txt, 'puntoB.txt');
   
-  write('ingrese un topico: '); readln(topico);
-  if (topico = 'a') then
-    begin
-      write('ingrese un num: '); readln(num);
-      actualizar(maestro, detalle, num);
-    end
-  else
-    puntoB(maestro, txt);
-    
+  actualizar(maestro, detalle);
+  puntoB(maestro, txt);   
 end.
+      
